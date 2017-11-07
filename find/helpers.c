72ff11ac7d38be32cd30524a1d24ac84b9d3fd55
Wil Trahan <wiltrahan@intracitygeeks.org>
2017-11-07 00:31:12 +0000

    begin pset3_find

--- find/helpers.c
+++ find/helpers.c
@@ -3,7 +3,7 @@
  *
  * Helper functions for Problem Set 3.
  */
- 
+
 #include <cs50.h>
 
 #include "helpers.h"
@@ -11,11 +11,31 @@
 /**
  * Returns true if value is in array of n values, else false.
  */
-bool search(int value, int values[], int n)
-{
+bool search(int value, int values[], int n) {
+
+    if(n < 1) {
+        return false;
+    }
     // TODO: implement a searching algorithm
+    int start = 0;
+    int end = n - 1;
+
+    while(end >= start) {
+        int center = (start + end) / 2;
+
+        if(value == values[center]) {
+            return true;
+        }
+        if(value == values[start]) {
+            return true;
+        }
+
+    }
+
     return false;
 }
+
+// Your implementation must return true if value is in values and false if value is not in values.
 
 /**
  * Sorts array of n values.
