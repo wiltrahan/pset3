f369d6e3c770fe81872f3c73c8d725cfe05c3cca
Wil Trahan <wiltrahan@intracitygeeks.org>
2017-11-07 22:54:33 +0000

    refactor search

--- find/helpers.c
+++ find/helpers.c
@@ -17,19 +17,23 @@ bool search(int value, int values[], int n) {
         return false;
     }
     // TODO: implement a searching algorithm
+    //start = left; end = right
     int start = 0;
     int end = n - 1;
 
     while(end >= start) {
         int center = (start + end) / 2;
 
-        if(value == values[center]) {
-            return true;
-        }
-        if(value == values[start]) {
+        if(value == values[start] || value == values[center] || value == values[end]) {
             return true;
         }
 
+        if(values[center] < value) {
+            start = center + 1;
+        }
+        else if(values[center] > value) {
+            end = center - 1;
+        }
     }
 
     return false;
@@ -43,5 +47,12 @@ bool search(int value, int values[], int n) {
 void sort(int values[], int n)
 {
     // TODO: implement a sorting algorithm
+    for(int i = 0; i < n - 2; i++) {
+        // int min = i;
+        if(values[i] > values[i + 1]) {
+            values[i] = values[i + 1];
+        }
+    }
     return;
 }
+
